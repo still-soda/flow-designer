4708048f0105eebc2d7df2ec829d1c0f7238b929
still_soda <951040628@qq.com>
2025-04-01 18:18:46 +0800

    feat: 添加用于 DEMO 的数据展示组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-   <FlowDesigner :options />
+   <FlowDesigner :options :flow-items />
 </template>
 
 <script setup lang="ts">
-import { Data, Plus, Minus, Close, Division } from '@icon-park/vue-next';
+import {
+   Data,
+   Plus,
+   Minus,
+   Close,
+   Division,
+   DataDisplay,
+} from '@icon-park/vue-next';
 import FlowDesigner from './components/FlowDesigner.vue';
 import type { FlowItem } from './types/flow-item.type';
 import type { Option, OptionActionPayload } from './types/option.type';
 import { genId } from './utils/uuid.utils';
 import { Point } from './utils/point.util';
+import {
+   AddItem,
+   DataSrouce,
+   MinusItem,
+   MultItem,
+   DivItem,
+   ResultItem,
+} from './extensions';
 
 // 右键菜单选项
 const createNewFlowItem = (
@@ -26,11 +41,25 @@ const createNewFlowItem = (
    });
 };
 
+const flowItems = {
+   source: DataSrouce,
+   add: AddItem,
+   minus: MinusItem,
+   mult: MultItem,
+   div: DivItem,
+   result: ResultItem,
+};
+
 const options: Option[] = [
    {
       icon: Data,
       title: '添加数据源',
       action: (payload) => createNewFlowItem(payload, 'source'),
+   },
+   {
+      icon: DataDisplay,
+      title: '添加结果展示',
+      action: (payload) => createNewFlowItem(payload, 'result'),
    },
    {
       icon: Plus,
